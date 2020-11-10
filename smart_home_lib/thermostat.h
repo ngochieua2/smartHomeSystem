b6b24badcaeeb100955d830a26ad1de8eee6bcbd
Ngoc Hieu Nguyen <nguny071@mymail.unisa.edu.au>
2020-11-10 23:08:01 +1030

    create thermostat class

--- smart_home_lib/thermostat.h
+++ smart_home_lib/thermostat.h
@@ -1,11 +1,30 @@
 #ifndef THERMOSTAT_H
 #define THERMOSTAT_H
 
+#include <smarthomedevices.h>
 
-class Thermostat
+class Thermostat : public SmartHomeDevices
 {
 public:
     Thermostat();
+    ~Thermostat();
+
+    //LastMeasurement()
+
+    //last5Measurement()
+
+    double setpoint(double desiredTemp);
+
+    //currentState;
+
+    void warmer( double amount);
+
+    void cooler(double amount);
+
+protected:
+    int updateFrequency{3};
+    double currentTemp{};
+    double lastTemp{};
 };
 
 #endif // THERMOSTAT_H
