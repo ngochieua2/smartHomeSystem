969fee1ab546e255526d45f4190ef61fde57df99
Ngoc Hieu Nguyen <nguny071@mymail.unisa.edu.au>
2020-11-10 23:07:42 +1030

    create spinkersystem class

--- smart_home_lib/sprinklersystem.cpp
+++ smart_home_lib/sprinklersystem.cpp
@@ -2,5 +2,24 @@
 
 SprinklerSystem::SprinklerSystem()
 {
+}
+
+SprinklerSystem::~SprinklerSystem()
+{
+}
+
+
+void SprinklerSystem::turnOn()
+{
+    OnOffStatus = true;
+}
+
+void SprinklerSystem::turnOff()
+{
+    OnOffStatus = false;
+}
+
+void SprinklerSystem::schedule(int delay, int duration)
+{
 
 }
--- smart_home_lib/sprinklersystem.h
+++ smart_home_lib/sprinklersystem.h
@@ -1,11 +1,30 @@
 #ifndef SPRINKLERSYSTEM_H
 #define SPRINKLERSYSTEM_H
 
+#include <smarthomedevices.h>
 
-class SprinklerSystem
+class SprinklerSystem: public SmartHomeDevices
 {
 public:
     SprinklerSystem();
+    ~SprinklerSystem();
+
+
+    void turnOn();
+
+    void turnOff();
+
+    void schedule(int delay, int duration);
+
+    // currentState : measurement
+
+    //waterUsage : measurement
+
+protected:
+    bool OnOffStatus{};
+    int updatedFrequency{5};
+    double waterConsumption{};
+
 };
 
 #endif // SPRINKLERSYSTEM_H
