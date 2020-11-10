0ba6752881c0e5d1a71e8df51566a383774c6e60
Ngoc Hieu Nguyen <nguny071@mymail.unisa.edu.au>
2020-11-10 23:04:33 +1030

    Implement basic LightSwitch header and cpp

--- smart_home_lib/lightswitch.cpp
+++ smart_home_lib/lightswitch.cpp
@@ -2,5 +2,42 @@
 
 LightSwitch::LightSwitch()
 {
+}
 
+LightSwitch::LightSwitch(QString id, QUrl Url)
+{
+    _Device_id = id;
+    _devideType = "LightSwitch";
+    _Url = Url;
+}
+
+LightSwitch::~LightSwitch()
+{
+}
+
+void LightSwitch::turnOn()
+{
+    OnOffState = true;
+}
+
+void LightSwitch::turnOff()
+{
+    OnOffState = false;
+}
+
+void LightSwitch::dim()
+{
+    if (brightnessLevel > 20){
+        brightnessLevel -= 20;
+    }
+}
+
+void LightSwitch::brighten()
+{
+    if (brightnessLevel < 100){
+        brightnessLevel += 20;
+    }
+    if (brightnessLevel > 100){
+        brightnessLevel = 100;
+    }
 }
--- smart_home_lib/lightswitch.h
+++ smart_home_lib/lightswitch.h
@@ -1,11 +1,31 @@
 #ifndef LIGHTSWITCH_H
 #define LIGHTSWITCH_H
 
+#include <smarthomedevices.h>
+#include <QString>
+#include <QUrl>
 
-class LightSwitch
+class LightSwitch : public SmartHomeDevices
 {
 public:
     LightSwitch();
+    LightSwitch(QString id, QUrl Url);
+    ~LightSwitch();
+
+    void turnOn();
+
+    void turnOff();
+
+    void dim();
+
+    void brighten();
+
+    //currentState : 2 measurements();
+
+protected:
+    bool OnOffState{};
+    int brightnessLevel{};
+
 };
 
 #endif // LIGHTSWITCH_H
