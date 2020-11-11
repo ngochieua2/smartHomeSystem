eb7c4305ef2c0310fc918b3d5b48d9f71ab563de
Thi Khanh Uyen Tran <traty145@mymail.unisa.edu.au>
2020-11-11 22:53:38 +1030

    Add ControllerProxy

---
+++ smart_home_lib/controllerproxy.h
@@ -0,0 +1,11 @@
+#ifndef CONTROLLERPROXY_H
+#define CONTROLLERPROXY_H
+
+
+class ControllerProxy
+{
+public:
+    ControllerProxy();
+};
+
+#endif // CONTROLLERPROXY_H
---
+++ smart_home_lib/realcontroller.h
@@ -0,0 +1,11 @@
+#ifndef REALCONTROLLER_H
+#define REALCONTROLLER_H
+
+
+class RealController
+{
+public:
+    RealController();
+};
+
+#endif // REALCONTROLLER_H
