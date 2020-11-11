5e670f502a585dc661cb7f1bac6ef6485d5abd7c
Ngoc Hieu Nguyen <nguny071@mymail.unisa.edu.au>
2020-11-11 14:50:24 +1030

    implement deviceinfo

--- smart_home_lib/deviceinfo.cpp
+++ smart_home_lib/deviceinfo.cpp
@@ -4,3 +4,30 @@ DeviceInfo::DeviceInfo()
 {
 
 }
+
+DeviceInfo::DeviceInfo(QString dName, QString dType, QUrl dUrl)
+    :_deviceName{dName}, _deviceType{dType}, _Url{dUrl}
+{}
+
+DeviceInfo::~DeviceInfo()
+{
+    _deviceName = "";
+    _deviceType = "";
+    _Url = "";
+}
+
+QString DeviceInfo::showDeviceInfo()
+{
+    QString info = "Device name: " + _deviceName + "\n"
+                   "Device type: " + _deviceType + "\n"
+                   "Url: " + _Url.toEncoded() + "\n";
+
+    if (_time.isNull()){
+        info += "\nLast seen: This is the first seen";
+    }
+    else {
+        info += "\nLast seen: " + _time.toString("H:m MMM dd yyyy");
+    }
+
+    return info;
+}
