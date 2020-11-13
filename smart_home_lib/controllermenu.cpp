1a940df46c753e067063300749b4c40f118eae07
Ngoc Hieu Nguyen <nguny071@mymail.unisa.edu.au>
2020-11-13 12:35:59 +1030

    Impelement controllermenu

--- smart_home_lib/controllermenu.cpp
+++ smart_home_lib/controllermenu.cpp
@@ -1,6 +1,58 @@
 #include "controllermenu.h"
+#include <QTextStream>
+#include <QUrl>
+#include <QCoreApplication>
 
-ControllerMenu::ControllerMenu()
+ControllerMenu::ControllerMenu(QTextStream &display, QTextStream &input, QObject *parent)
+  : QObject{parent}, _display{display}, _input{input}
 {
+    //_lightSwitchMenu = new  LightSwitchMenu(display, input);
+}
+
+void ControllerMenu::run(RealController* controller){
+    _controller = controller;
+    while (true) {
+        _display << "Welcome to controller menu" << endl
+                 << "Name controller: " << _controller->getID() << endl
+                 << "What would you like to do" << endl
+                 << "1.Show all register devices" << endl
+                 << "2.Unregister device" << endl
+                 << "3.Controll device" << endl
+                 << "4." << endl
+                 << "Type (b) to back" << endl;
+
+        QString stringInput;
+        stringInput = _input.readLine();
+        if(stringInput == "1"){
+            // get info
+            for(int i = 0; i < _controller->getLightSwitchProxyList().size(); ++i){
+                emit showRegisterDevice(_controller->getLightSwitchProxyList().at(i));
+                //_lightSwitchMenu->showRegisterDevice(_controller->getLightSwitchProxyList().at(i));
+            }
+            //Show data
+            _display << endl;
+            _display << _controller->registerDevice();
+            _display << endl;
+
+        }
+        else if (stringInput == "2") {
+            _display << "2 event" <<endl;
+
+        }
+        else if (stringInput == "3") {
+            _display << "3 event" <<endl;
+        }
+        else if (stringInput == "4") {
+            _display << "4 event" <<endl;
+        }
+        else if (stringInput == "b") {
+            break;
+        }
+        else {
+            _display << "Invalid option, please choose other" <<endl;
+        }
+    }
+
+
 
 }
--- smart_home_lib/controllermenu.h
+++ smart_home_lib/controllermenu.h
@@ -1,11 +1,33 @@
 #ifndef CONTROLLERMENU_H
 #define CONTROLLERMENU_H
 
+#include <QString>
+#include <QStringList>
+#include <QTextStream>
+#include <realcontroller.h>
+#include <lightswitchmenu.h>
 
-class ControllerMenu
+
+class ControllerMenu : public QObject
 {
+    Q_OBJECT
 public:
-    ControllerMenu();
+    explicit ControllerMenu(QTextStream &display, QTextStream &input, QObject *parent = nullptr);
+    virtual ~ControllerMenu() = default;
+
+    void run(RealController* controller);
+
+signals:
+    void showRegisterDevice(LightSwitchProxy*);
+
+private:
+  QTextStream &_display;
+  QTextStream &_input;
+
+  RealController* _controller{nullptr};
+
+  LightSwitchMenu* _lightSwitchMenu{nullptr};
+
 };
 
 #endif // CONTROLLERMENU_H
