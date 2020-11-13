bb227d169fcd1e3c73cc59fe4d85924832099dbd
Ngoc Hieu Nguyen <nguny071@mymail.unisa.edu.au>
2020-11-13 12:38:32 +1030

    Impelement realcontroller

--- smart_home_lib/realcontroller.cpp
+++ smart_home_lib/realcontroller.cpp
@@ -2,5 +2,73 @@
 
 RealController::RealController()
 {
+}
+
+RealController::RealController(QString id, QUrl Url)
+{
+    _controller_id = id;
+    _controller_Url = Url;
+}
+
+RealController::~RealController()
+{
+    for(int i = 0 ; i <_lightSwitchProxyList.size(); ++i){
+        delete _lightSwitchProxyList.at(i);
+    }
 
 }
+
+QString RealController::getID()
+{
+    return _controller_id;
+}
+
+void RealController::registerDevice(QString name, QString type, QUrl URL)
+{
+    if(type == "lightSwitch"){
+        _lightSwitchProxyList.append(new LightSwitchProxy(name, URL));
+    }
+    else {
+
+    }
+}
+
+QString RealController::registerDevice()
+{
+    QString data{};
+    if(_deviceInfoList.isEmpty()){
+        data = "There is no register devices\n"
+                "Please come back to main Menu and register new device\n";
+    }
+    else {
+        for(int i = 0; i <_deviceInfoList.size(); i++ ){
+            data +=  QString::number(i+1) + "." + _deviceInfoList.at(i)->showDeviceInfo();
+        }
+    }
+    _deviceInfoList.clear();
+    return data;
+}
+
+LightSwitchProxy *RealController::getLightSwitchProxy()
+{
+    return _lightSwitchProxyList.last();
+}
+
+QList<LightSwitchProxy *> RealController::getLightSwitchProxyList()
+{
+    return _lightSwitchProxyList;
+}
+
+void RealController::receiveDeviceInfo(DeviceInfo *deviceInfo)
+{
+    _deviceInfoList.append(deviceInfo);
+}
+
+QList<DeviceInfo *> RealController::getDeviceInfoList()
+{
+    return _deviceInfoList;
+}
+
+
+
+
--- smart_home_lib/realcontroller.h
+++ smart_home_lib/realcontroller.h
@@ -1,11 +1,39 @@
 #ifndef REALCONTROLLER_H
 #define REALCONTROLLER_H
 
+#include <smarthomecontroller.h>
+#include <lightswitchproxy.h>
 
-class RealController
+class LightSwitchProxy;
+
+class RealController : SmartHomeController
 {
 public:
     RealController();
+    RealController(QString id, QUrl Url);
+    ~RealController();
+
+    QString getID();
+
+    void registerDevice(QString name, QString type, QUrl URL);
+
+    QString registerDevice();
+
+    LightSwitchProxy* getLightSwitchProxy();
+
+    QList<LightSwitchProxy*> getLightSwitchProxyList();
+
+    void receiveDeviceInfo(DeviceInfo *deviceInfo) override;
+
+    QList<DeviceInfo*> getDeviceInfoList();
+
+
+private:
+    //light proxy
+    QList <LightSwitchProxy*> _lightSwitchProxyList{};
+
+    QList<DeviceInfo*> _deviceInfoList{};
+
 };
 
 #endif // REALCONTROLLER_H
