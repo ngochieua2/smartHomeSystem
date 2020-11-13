4b4d48515008f7d45e36a8e49d566c933ff25d5e
Ngoc Hieu Nguyen <nguny071@mymail.unisa.edu.au>
2020-11-13 12:37:59 +1030

    Impelement lightswitchproxy

--- smart_home_lib/lightswitchproxy.cpp
+++ smart_home_lib/lightswitchproxy.cpp
@@ -1,6 +1,43 @@
 #include "lightswitchproxy.h"
 
-LightSwitchProxy::LightSwitchProxy()
+LightSwitchProxy::LightSwitchProxy(QString id, QUrl Url)
 {
+    _device_id = id;
+    _devideType = "lightSwitch";
+    _deviceUrl = Url;
+}
 
+LightSwitchProxy::~LightSwitchProxy()
+{
+    delete _realLightSwitch;
+}
+
+void LightSwitchProxy::turnOn()
+{
+    _realLightSwitch->turnOn();
+}
+
+void LightSwitchProxy::turnOff()
+{
+    _realLightSwitch->turnOff();
+}
+
+void LightSwitchProxy::dim()
+{
+    _realLightSwitch->dim();
+}
+
+void LightSwitchProxy::brighten()
+{
+    _realLightSwitch->brighten();
+}
+
+void LightSwitchProxy::passRealLightSwitch(RealLightSwitch *realLightSwitch)
+{
+    _realLightSwitch = realLightSwitch;
+}
+
+void LightSwitchProxy::getDeviceInfo()
+{
+    _realLightSwitch->getDeviceInfo();
 }
--- smart_home_lib/lightswitchproxy.h
+++ smart_home_lib/lightswitchproxy.h
@@ -1,11 +1,32 @@
 #ifndef LIGHTSWITCHPROXY_H
 #define LIGHTSWITCHPROXY_H
 
+#include <lightswitch.h>
+#include <reallightswitch.h>
 
-class LightSwitchProxy
+class RealLightSwitch;
+
+class LightSwitchProxy : public LightSwitch
 {
 public:
     LightSwitchProxy();
+    LightSwitchProxy(QString id, QUrl URL);
+    virtual ~LightSwitchProxy();
+
+    void turnOn() override;
+
+    void turnOff() override;
+
+    void dim() override;
+
+    void brighten() override;
+
+    void passRealLightSwitch(RealLightSwitch* realLightSwitch);
+
+    void getDeviceInfo() override;
+
+private:
+    RealLightSwitch* _realLightSwitch{nullptr};
 };
 
 #endif // LIGHTSWITCHPROXY_H
