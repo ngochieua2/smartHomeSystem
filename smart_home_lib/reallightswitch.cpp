0408d21d1675d4df905f2a44f4fc2f61c88bbcaf
Ngoc Hieu Nguyen <nguny071@mymail.unisa.edu.au>
2020-11-13 12:38:55 +1030

    Impelement reallightswitch

--- smart_home_lib/reallightswitch.cpp
+++ smart_home_lib/reallightswitch.cpp
@@ -4,3 +4,61 @@ RealLightSwitch::RealLightSwitch()
 {
 
 }
+
+RealLightSwitch::RealLightSwitch(QString id, QUrl URL)
+{
+    _device_id = id;
+    _devideType = "light Switch";
+    _deviceUrl = URL;
+    _deviceInfo = new DeviceInfo(_device_id,_devideType,_deviceUrl);
+}
+
+RealLightSwitch::~RealLightSwitch()
+{
+
+}
+
+void RealLightSwitch::turnOn()
+{
+    OnOffState = true;
+}
+
+void RealLightSwitch::turnOff()
+{
+    OnOffState = false;
+}
+
+void RealLightSwitch::dim()
+{
+    if (brightnessLevel > 20){
+        brightnessLevel -= 20;
+    }
+}
+
+void RealLightSwitch::brighten()
+{
+    if (brightnessLevel < 100){
+        brightnessLevel += 20;
+    }
+    if (brightnessLevel > 100){
+        brightnessLevel = 100;
+    }
+}
+
+void RealLightSwitch::createControllerProxy()
+{
+    _controllerProxy = new ControllerProxy();
+}
+
+ControllerProxy *RealLightSwitch::getControllerProxy()
+{
+    return _controllerProxy;
+}
+
+void RealLightSwitch::getDeviceInfo()
+{
+    _controllerProxy->receiveDeviceInfo(_deviceInfo);
+    _deviceInfo = new DeviceInfo(_device_id,_devideType,_deviceUrl);
+    _deviceInfo->updateTime();
+
+}
--- smart_home_lib/reallightswitch.h
+++ smart_home_lib/reallightswitch.h
@@ -1,11 +1,41 @@
 #ifndef REALLIGHTSWITCH_H
 #define REALLIGHTSWITCH_H
 
+#include <lightswitch.h>
+#include <controllerproxy.h>
 
-class RealLightSwitch
+class ControllerProxy;
+
+class RealLightSwitch : public LightSwitch
 {
 public:
     RealLightSwitch();
+    RealLightSwitch(QString id, QUrl URL);
+    virtual ~RealLightSwitch();
+
+    void turnOn() override;
+
+    void turnOff() override;
+
+    void dim() override;
+
+    void brighten() override;
+
+    void createControllerProxy();
+
+    ControllerProxy* getControllerProxy();
+
+    void getDeviceInfo() override;
+
+private:
+    bool OnOffState{};
+    int brightnessLevel{};
+
+    ControllerProxy* _controllerProxy{};
+
+    DeviceInfo* _deviceInfo{nullptr};
+
+
 };
 
 #endif // REALLIGHTSWITCH_H
